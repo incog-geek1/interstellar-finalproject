7dfe7a1c21e4652cb8cb8cb901a74feb40a55a1f
incog-geek1 <151454755+incog-geek1@users.noreply.github.com>
2024-03-15 08:06:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-import express from '.express'
-
+import express from 'express'
+import basicAuth from 'express-basic-auth'
 import http from 'node:http'
-
+import { createBareServer } from '@tomphttp/bare-server-node'
 import path from 'node:path'
-
+import cors from 'cors'
 import config from './config.js'
-
+const __dirname = process.cwd()
 const server = http.createServer()
 const app = express(server)
 const bareServer = createBareServer('/v/')
 const PORT = process.env.PORT || 8080
 if (config.challenge) {
-  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users));
-  console.log('Passwords are: ' + Object.values(config.users));
+  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
+  console.log('Passwords are: ' + Object.values(config.users))
 
-  app.use(basicAuth({
-    users: config.users,
-    challenge: true,
-  }));
+  app.use(
+    basicAuth({
+      users: config.users,
+      challenge: true,
+    })
+  )
 }
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -33,7 +35,7 @@ if (config.routes !== false) {
     { path: '/1', file: 'go.html' },
     { path: '/', file: 'index.html' },
   ]
-  
+
   routes.forEach((route) => {
     app.get(route.path, (req, res) => {
       res.sendFile(path.join(__dirname, 'static', route.file))
@@ -41,35 +43,34 @@ if (config.routes !== false) {
   })
 }
 if (config.local !== false) {
-    app.get('/y/*', cors({ origin: false }), (req, res, next) => {
-      const baseUrl = 'https://raw.githubusercontent.com/ypxa/y/main'
-      fetchData(req, res, next, baseUrl)
-    })
-  
-    app.get('/f/*', cors({ origin: false }), (req, res, next) => {
-      const baseUrl = 'https://raw.githubusercontent.com/4x-a/x/fixy'
-      fetchData(req, res, next, baseUrl)
-    })
-  }
-  
-  const fetchData = async (req, res, next, baseUrl) => {
-    try {
-      const reqTarget = `${baseUrl}/${req.params[0]}`
-      const asset = await fetch(reqTarget)
-  
-      if (asset.ok) {
-        const data = await asset.arrayBuffer()
-        res.end(Buffer.from(data))
-      } else {
-        next()
-      }
-    } catch (error) {
-      console.error('Error fetching:', error)
-      next(error)
-    }
-  }
+  app.get('/y/*', (req, res, next) => {
+    const baseUrl = 'https://raw.githubusercontent.com/ypxa/y/main'
+    fetchData(req, res, next, baseUrl)
+  })
 
-  server.on('request', (req, res) => {
+  app.get('/f/*', (req, res, next) => {
+    const baseUrl = 'https://raw.githubusercontent.com/4x-a/x/fixy'
+    fetchData(req, res, next, baseUrl)
+  })
+}
+
+const fetchData = async (req, res, next, baseUrl) => {
+  try {
+    const reqTarget = `${baseUrl}/${req.params[0]}`
+    const asset = await fetch(reqTarget)
+
+    if (asset.ok) {
+      const data = await asset.arrayBuffer()
+      res.end(Buffer.from(data))
+    } else {
+      next()
+    }
+  } catch (error) {
+    console.error('Error fetching:', error)
+    next(error)
+  }
+}
+server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res)
   } else {
